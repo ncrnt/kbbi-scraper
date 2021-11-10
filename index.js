f34c361aaff61ff37aa1e44c79dd25d4ab0c4eb6
Nathz <75367930+Yuzaki01@users.noreply.github.com>
2021-11-10 21:21:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ const cheerio = require('cheerio');
 const baseUrl = require('./helper.json').base_url;
 
 module.exports = (query) => {
-    if(!query) throw new Error('Masukkan kata!');
+    if (!query) throw new Error('Masukkan kata!');
     return new Promise((resolve, reject) => {
+        let dataError = {
+            message: 'Kata tidak ditemukan!',
+            error: true,
+            data: {}
+        }
+
+        var _format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+        if(_format.test(query)) return resolve(dataError);
+
         axios.get(`${baseUrl}/entri/${query}`)
             .then((response) => {
                 if (response.status === 200) {
                     const res = response.data;
                     const $ = cheerio.load(res);
                     let hasil = {};
+
+                    hasil.arti = null
+                    hasil.title = null
 
                     $('.container').filter(function (a, b) {
                         hasil.title = $(this).find('h2').text().trim() == ''
@@ -22,25 +34,19 @@ module.exports = (query) => {
                                 : $(this).find('li').eq(0).text().replace(/\s+/g, ' ').trim()
                     });
 
-                    const finalHasil = {
+                    let finalHasil = {
                         message: 'Berhasil!',
                         error: false,
                         data: hasil
                     }
 
-                    const dataError = {
-                        message: 'Kata tidak ditemukan!',
-                        error: true,
-                        data: {}
-                    }
-
                     if (hasil.arti !== 'memudahkan pencarian Anda melalui berbagai fitur yang hanya tersedia bagi pengguna terdaftar') {
-                        if(hasil.arti === null) return resolve(dataError);
+                        if (hasil.arti === null || hasil.title === null) return resolve(dataError);
                         resolve(finalHasil)
                     } else {
                         resolve(dataError);
                     }
                 }
-            }).catch(console.error());
+            }).catch(console.error())
     })
 }
